f257ee006d165b47a24e169fabc79a596ff3d1ce
Madison Bertis <madisonbertis@Madisons-iMac.local>
2021-04-13 15:01:02 -0700

    fixed Injectable import

--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Injectable, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { catchError } from "rxjs/internal/operators";
 import { HttpClient, HttpHeaders, HttpErrorResponse } from "@angular/common/http";
 import { Observable, throwError } from "rxjs";
